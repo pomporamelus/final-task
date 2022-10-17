d42f6e0e4101debc063d345b8a8dbd00b629f3ab
pomporamelus <emir.zhakypov@gmail.com>
2022-10-17 22:07:17 +0600

    smth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt'
 import { UserEntity } from 'src/user/entity';
 import { createUserDto, loginUserDto } from 'src/user/dto';
 
@@ -24,11 +24,11 @@ export class AuthService {
         HttpStatus.BAD_REQUEST,
       );
     }
-    const passwordEqual = await bcrypt.compare(dto.password, user.password);
-    if (passwordEqual) {
-      console.log(user);
-      return this.generateToken(user);
-    }
+    // const passwordEqual = await bcrypt.compare(dto.password, user.password);
+    // if (passwordEqual) {
+    //   console.log(user);
+    //   return this.generateToken(user);
+    // }
     throw new HttpException(
       'password is incorrect',
       HttpStatus.BAD_REQUEST,
@@ -43,7 +43,8 @@ export class AuthService {
         HttpStatus.BAD_REQUEST,
       );
     }
-    const hashPassword = await bcrypt.hash(dto.password, 5);
+    // const hashPassword = await bcrypt.hash(dto.password, 5);
+    let hashPassword = '123'
     const updatedUser = { ...dto, password: hashPassword };
     Object.assign(dto, updatedUser);
     console.log(dto);
